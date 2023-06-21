14eb260068220a65bfd82fc49209a46cb0806268
HORIZON\joseph.li <joseph.li@hzn.com.tw>
2023-06-21 11:08:41 +0800

    feat(展望與傳承): 完成

--- src/components/PlanningAndInheritance.vue
+++ src/components/PlanningAndInheritance.vue
@@ -20,27 +20,35 @@
       </a-carousel>
       <div class="plan">
         <div class="text">
-          <h3><span>國賓與華泰飯店大規模改建</span><span>再造國際星級地段</span></h3>
-          <p>
+          <h3 class="fadeInElement">
+            <span>國賓與華泰飯店大規模改建</span><span>再造國際星級地段</span>
+          </h3>
+          <p class="fadeInElement">
             中山北路的新遠見，始於政經商軸的不凡，承襲國際層峰的視野。傳產企業總部匯聚，千億企業總部與國賓飯店危老改建，一世紀的黃金歲月，將再次繁華盛放，地段增值鍍金，企業智慧與富裕傳承淵源長流。
           </p>
           <div class="pic pc_pic">
-            <img src="@/assets/images/planning_and_inheritance/s4-1.jpg" alt="" />
+            <img
+              src="@/assets/images/planning_and_inheritance/s4-1.jpg"
+              alt=""
+            />
           </div>
           <div>
-            <h3>上海、陽信雙總部 金融重磅版圖</h3>
-            <p>
+            <h3 class="fadeInElement">上海、陽信雙總部 金融重磅版圖</h3>
+            <p class="fadeInElement">
               「上海商銀總部」與「陽信銀行總部」2大金庫高瞻對望，鄰近國賓飯店改建、大同總部開發...等重大開發領銜，定錨金融特區，燙金門牌與未來榮景潛力無限。
             </p>
           </div>
           <div>
-            <h3>北美館擴建計劃 打造森林美術館</h3>
-            <p>
+            <h3 class="fadeInElement">北美館擴建計劃 打造森林美術館</h3>
+            <p class="fadeInElement">
               「林中蘊藝，藝中觀林」為設計理念，以北美館為核心，串聯花博公園美術園區、新生公園與中山北路等，打造一條文化藝術、自然綠意與都市景觀交織而成的文化綠廊，為北市種下一座蘊藏豐沛藝文能量的森林藝術園區。
             </p>
           </div>
           <div class="pic decoration">
-            <img src="@/assets/images/planning_and_inheritance/ill-10.png" alt="" />
+            <img
+              src="@/assets/images/planning_and_inheritance/ill-10.png"
+              alt=""
+            />
           </div>
         </div>
       </div>
@@ -48,39 +56,52 @@
     <div class="inheritance">
       <div class="bg">
         <div class="pic">
-          <img src="" alt="" />
+          <img src="@/assets/images/planning_and_inheritance/kite.gif" alt="" />
+          <img
+            src="@/assets/images/planning_and_inheritance/ill-11.png"
+            alt=""
+          />
         </div>
         <div class="text">
-          <h2>Home of The Royal</h2>
-          <h2>家族深耕 美好傳承</h2>
-          <h3>永隆鋁業攜手協律 起家厝到新地標</h3>
-          <p>
+          <h2 class="fadeInElement">Home of The Royal</h2>
+          <h2 class="fadeInElement">家族深耕 美好傳承</h2>
+          <h3 class="fadeInElement">永隆鋁業攜手協律 起家厝到新地標</h3>
+          <p class="fadeInElement">
             「從中山北到全世界，回眸千百回，世代傳承的起家厝。」
             林森北第一排美地，原為永隆鋁業家族的事業起家厝，超過一甲子的台灣隱形冠軍，早年為大同電子夥伴，台灣家庭必備的大同電鍋即是出自永隆鋁業之手，你我熟悉的身影一直在身邊。「中山簡美」以家族之名，承載時代記憶與土地故事，多年等候，與協律共譜，從心打造回家的路，構築老家到新家的心距離。
           </p>
-
-          <h2>建築初心 美麗緣起</h2>
-          <h3>以家族傳承「真善美」 成就經典好宅</h3>
-          <p>
+          <hr />
+          <h2 class="fadeInElement">建築初心 美麗緣起</h2>
+          <h3 class="fadeInElement">以家族傳承「真善美」 成就經典好宅</h3>
+          <p class="fadeInElement">
             因土地結緣，一段美麗的起源，永隆鋁業大家長，擔任教會神職的郭教師，長年事奉於基督教會教導恩賜，追隨宗教社會公益，本著家族先輩的信念，以阿嬤之名「簡美」傳承人文底蘊，期以建築存記這份美好，成就百年不朽的願景，延續家的想望。
           </p>
         </div>
       </div>
-      <div class="text">
-        <h2>土地累積 成就奇蹟</h2>
-        <h2>城市築夢 建築理想</h2>
-        <p>
-          中山北路、林森北路，一路繁華多少台灣名門企業於此發跡，伴隨台灣經濟起飛，日治時代的「宮前町」即是今日中山國小的範圍，引領國際潮流，孕育有故事的傳奇街區，爬梳土地與鄰里關係，以優雅的節奏與建築理想，詮釋生活，重新定義家的樣子，從生活長出來的建築，正在這座城市築夢。
-        </p>
-
-        <h3>衷於完美 源自不必要的堅持</h3>
-        <p>
-          協律：「好宅比豪宅更重要。」「產品的熱情與堅持來自於不妥協。」，面對營建成本攀高與市場洗牌，產品如何定位，長住久安、好看保值等皆是工夫。適切的規劃，滿足都會小家庭的需求，好住好用好維護，以中大坪數的規格起造，發揮基地與環境特色！建築本真，愛上源自生活的美麗
-          。
-        </p>
-        <img src="" alt="" />
+      <div class="despcrition">
+        <div class="text">
+          <h2 class="fadeInElement">土地累積 成就奇蹟</h2>
+          <h2 class="fadeInElement">城市築夢 建築理想</h2>
+          <p class="fadeInElement">
+            中山北路、林森北路，一路繁華多少台灣名門企業於此發跡，伴隨台灣經濟起飛，日治時代的「宮前町」即是今日中山國小的範圍，引領國際潮流，孕育有故事的傳奇街區，爬梳土地與鄰里關係，以優雅的節奏與建築理想，詮釋生活，重新定義家的樣子，從生活長出來的建築，正在這座城市築夢。
+          </p>
+          <hr />
+          <h3 class="fadeInElement">衷於完美 源自不必要的堅持</h3>
+          <p class="fadeInElement">
+            協律：「好宅比豪宅更重要。」「產品的熱情與堅持來自於不妥協。」，面對營建成本攀高與市場洗牌，產品如何定位，長住久安、好看保值等皆是工夫。適切的規劃，滿足都會小家庭的需求，好住好用好維護，以中大坪數的規格起造，發揮基地與環境特色！建築本真，愛上源自生活的美麗
+            。
+          </p>
+          <div class="pic">
+            <img
+              src="@/assets/images/planning_and_inheritance/ill-12.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="pic">
+          <img src="@/assets/images/planning_and_inheritance/s6.jpg" alt="" />
+        </div>
       </div>
-      <div class="pic"></div>
     </div>
   </section>
 </template>
@@ -100,6 +121,18 @@
   h2 {
     font-size: 18px;
     font-weight: bold;
+
+    @include desktops {
+      font-size: 18px;
+    }
+
+    @include smallScreen {
+      font-size: 20px;
+    }
+
+    @include middleScreen {
+      font-size: 30px;
+    }
   }
 
   h3 {
@@ -127,7 +160,7 @@
       }
     }
 
-    span+span {
+    span + span {
       margin-bottom: 30px;
 
       @include desktops {
@@ -157,7 +190,7 @@
 
 .wrapper {
   margin-bottom: 48px;
-  
+
   @include desktops {
     display: flex;
     margin-bottom: 160px;
@@ -208,6 +241,140 @@
 
     @include desktops {
       display: block;
+    }
+  }
+
+  h3.show {
+    @include fadeAndMoveUpAnimation(0.2s);
+  }
+
+  p.show {
+    @include fadeAndMoveUpAnimation(0.4s);
+  }
+}
+
+hr {
+  @include desktops {
+    display: none;
+  }
+}
+.inheritance {
+  margin-bottom: 48px;
+
+  @include desktops {
+    margin-bottom: 160px;
+  }
+
+  .bg {
+    margin-bottom: 48px;
+    padding-bottom: 40px;
+    background-image: url("@/assets/images/planning_and_inheritance/s5.jpg");
+    background-position: center center;
+    background-size: cover;
+    background-color: rgba(0, 0, 0, 0.2);
+    background-blend-mode: multiply;
+    position: relative;
+
+    @include desktops {
+      display: flex;
+      align-items: flex-end;
+      padding: 15% 5% 2%;
+      margin-bottom: 160px;
+    }
+
+    .pic {
+      position: relative;
+      width: 40%;
+      margin: 0 auto;
+      padding: 40px 0;
+
+      @include desktops {
+        position: absolute;
+        padding: 0;
+        width: 20%;
+        right: 10%;
+        bottom: -10%;
+      }
+
+      img {
+        &:first-child {
+          position: absolute;
+        }
+      }
+    }
+
+    > .text {
+      color: #fff;
+
+      @include desktops {
+        width: 50%;
+      }
+
+      h2,
+      h3 {
+        color: #fff;
+      }
+
+      p {
+        font-weight: bold;
+      }
+
+      h2.show {
+        @include fadeAndMoveUpAnimation();
+
+        &:first-child {
+          @include fadeAndMoveDownAnimation();
+        }
+      }
+
+      h3.show {
+        @include fadeAndMoveUpAnimation(0.4s);
+      }
+
+      p.show {
+        @include fadeAndMoveUpAnimation(0.6s);
+      }
+    }
+  }
+
+  .despcrition {
+    @include desktops {
+      display: flex;
+      align-items: center;
+
+      .text {
+        width: 50%;
+        padding: 10%;
+      }
+      .pic {
+        width: 50%;
+      }
+    }
+    > .text {
+      > .pic {
+        margin-left: 25%;
+        width: 70%;
+      }
+    }
+
+    h2.show {
+      @include fadeAndMoveUpAnimation(0.2s);
+
+      &:first-child {
+        @include fadeAndMoveUpAnimation();
+      }
+    }
+    h3.show {
+      @include fadeAndMoveUpAnimation(0.6s);
+    }
+
+    p.show {
+      &:nth-child(3) {
+        @include fadeAndMoveUpAnimation(0.4s);
+      }
+      &:nth-child(6) {
+        @include fadeAndMoveUpAnimation(0.8s);
+      }
     }
   }
 }
